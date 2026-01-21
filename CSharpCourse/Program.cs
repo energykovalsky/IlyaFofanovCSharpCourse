ccabdfddd0aaacc8862e72e26e60a6575d7e8a1b
Andriy Kovalsky <energy.kovalsky@gmail.com>
2026-01-21 16:58:19 +0200

    Add project files.

---
+++ CSharpCourse/Program.cs
@@ -0,0 +1,161 @@
+﻿using System.Formats.Asn1;
+
+namespace CSharpCourse
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+
+        static void IncrementDecrementDemo()    //016 Алгебраические операции.mp4 (1)
+        {
+            int x = 1;
+            x = x + 1;
+            Console.WriteLine(x);
+
+            x++;
+            Console.WriteLine(x);
+
+            ++x;
+            Console.WriteLine(x);
+
+            x = x - 1;
+            x--;
+            --x;
+
+            Console.WriteLine(x);
+
+            Console.WriteLine("Learn about increments");
+            Console.WriteLine($"Last x state is {x}");
+
+            int j = x++;
+            Console.WriteLine(j);
+            Console.WriteLine(x);
+
+            j = ++x;
+            Console.WriteLine(j);
+            Console.WriteLine(x);
+
+            x += 2;
+            // x = x + 2;
+
+            j -= 2;
+            // j = j - 2;
+
+            Console.WriteLine(j);
+        }
+        static void Overflow()
+        {
+            checked
+            {
+                uint x = uint.MaxValue;
+
+                Console.WriteLine(x);
+
+                x++;
+                Console.WriteLine(x);
+
+                x--;
+                Console.WriteLine(x);
+            }
+        }
+        static void VariablesScope()            //014 Область видимости переменных.mp4
+        {
+            var a = 1;
+            {
+                var b = 2;
+                {
+                    var c = 3;
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                //Console.WriteLine(c);
+            }
+            Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+        }
+        static void Literals()                  //013 Литералы.mp4
+        {
+            int x = 0b11;
+            int y = 0b1001;
+            int k = 0b10001001;
+            int m = 0b1000_1001;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+
+            x = 0x1F;
+            y = 0xFF0D;
+            k = 0x1FAB30EF;
+            m = 0x1FAB_30EF;
+
+            Console.WriteLine();
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+
+            Console.WriteLine();
+
+            Console.WriteLine(4.5e2);
+            Console.WriteLine(3.1E-1);
+
+            Console.WriteLine();
+
+            Console.WriteLine('\x78');
+            Console.WriteLine('\x5A');
+
+            Console.WriteLine('\u0420');
+            Console.WriteLine('\u0421');
+        } 
+        static void Variables()                 //012 Создаём переменные.mp4
+        {
+            int x = -1;
+
+            int y;
+            y = 2;
+
+            //Int32 x1 = -1;
+
+            //uint z = -1;
+
+            float f = 1.1f;
+
+            double d = 2.3;
+
+            int x2 = 0;
+            int x3 = new int();
+
+            var a = 1;
+            var b = 1.2;
+
+            //Dictionary<int, string> dict = new Dictionary<int, string>();
+            //var dict = new Dictionary<int, string>();
+
+            //var v;
+
+            decimal money = 3.0m;
+
+            char @char = 'A';
+            string name = "John";
+
+            bool canDrive = true;
+            bool canDraw = false;
+
+            object obj1 = 1;
+            object obj2 = "obj2";
+
+            Console.WriteLine(a);
+            Console.WriteLine(name);
+        }
+    }
+}
