42b1ba6d0aa28a656ed155faac9f3b41b509bcb3
Andriy Kovalsky <energy.kovalsky@gmail.com>
2026-01-23 10:48:20 +0200

    Added CSharpCourse tasks

--- CSharpCourse/Program.cs
+++ CSharpCourse/Program.cs
@@ -1,13 +1,405 @@
-﻿namespace CSharpCourse
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
+﻿using System.Security.Cryptography;
+using System.Text;
 
+namespace CSharpCourse
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            
         }
 
-        static void IncrementDecrementDemo()    //016 Алгебраические операции.mp4 (1)
+        static void DateTimeIntro()
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString()); //11.07.2019 19:04:02
+
+            Console.WriteLine($"It's {now.Date}, {now.Hour}:{now.Minute}"); /It's 11.07.2019 0:00:00, 19:4
+
+            DateTime dt = new DateTime(2016, 2, 28);
+            DateTime newDt = dt.AddDays(1);
+
+            Console.WriteLine(newDt); //29.02.2016 0:00:00
+
+            TimeSpan ts = now - dt;
+            // ts = now.Subtract(dt);
+            Console.WriteLine(ts.Days); //1229
+
+        }
+        static void IntroToArrays()
+        {
+            int[] a1;
+            a1 = new int[10];
+            
+            int[] a2 = new int[5];
+
+            int[] a3 = new int[5] { 1, 2, 3, 4, 5 };
+            int[] a4 = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(a4[0]); // 1
+
+            int number = a4[4];
+            Console.WriteLine(number); //5
+
+            a[4] = 6;
+            Console.WriteLine(a4[4]); // 6
+
+            Console.WriteLine(a4.Length); // 5
+            Console.WriteLine(a4[a4.Length - 1]); //6
+
+            string s1 = "abcdefgh";
+            char first = s1[0]; //a
+            char last = s1[s1.Length - 1]; //h
+
+            Console.WriteLine($"First:{first}. Last:{last}");
+        }
+        static void MathDemo()                  //027 Класс Math.mp4                        
+        {
+            Console.WriteLine(Math.Pow(2, 3));
+            Console.WriteLine(Math.Sqrt(9));
+            Console.WriteLine(Math.Sqrt(8));
+
+            Console.WriteLine(Math.Round(1.7));
+            Console.WriteLine(Math.Round(1.4));
+
+            Console.WriteLine();
+
+            Console.WriteLine(Math.Round(2.5));
+            Console.WriteLine(Math.Round(2.5, MidpointRounding.AwayFromZero));
+            Console.WriteLine(Math.Round(2.5, MidpointRounding.ToEven));
+        }
+        static void Comments()                  //026 Комментарии.mp4                       
+        {
+            //a single-line comment
+
+            /*
+             * Multi-line comment
+             * We can write here many words
+             */
+
+            //describe hows and whys! not whats!
+
+            int a = 1;
+
+            //increment a by 1 - bad comment, this code is self-evident
+
+            //we need to tweak the index to match the expected outcome
+            a++;
+        }
+        static void CastingAndParsing()         //025 Приведение типов и парсинг.mp4        
+        {
+            byte b = 3; // 0000 0011
+            int i = b; // 0000 0000 0000 0000 0000 0000 0000 0011
+            long l = i; // 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0011
+
+            float f = i; //3.0
+
+            Console.WriteLine(f);
+
+            b = (byte)i;
+            Console.WriteLine(b);
+
+            i = (int)f;
+            Console.WriteLine(i);
+
+            f = 3.1f;
+            i = (int)f;
+            Console.WriteLine(i);
+
+            string str = "1";
+            //i = (int)str;
+            i = int.Parse(str);
+            Console.WriteLine($"Parsed i={i}");
+
+            int x = 5;
+            int result = x / 2;
+            Console.WriteLine(result);
+
+            double result2 = (double)x / 2;
+            Console.WriteLine(result2);
+        }
+        static void ConsoleBasics()             //024 Работаем с консолью.mp4               
+        {
+            //Console.WriteLine("Hi, please tell me your name");
+
+            //string name = Console.ReadLine();
+            //string sentence = $"Your name is {name}";
+            //Console.WriteLine(sentence);
+
+            Console.WriteLine("Hi, please tell me your age.");
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+
+            string sentence = $"Your age is {age}";
+            Console.WriteLine(sentence);
+
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Write("New Style ");
+            Console.Write("New Style\n");
+        }
+        static void ComparingStrings()          //023 Сравнение строк.mp4                   
+        {
+            //string str1 = "abcde";
+            //string str2 = "abcde";
+
+            //bool areEqual = str1 == str2;
+            //Console.WriteLine(areEqual);
+
+            //areEqual = string.Equals(str1, str2, StringComparison.Ordinal);
+            //Console.WriteLine(areEqual);
+
+            string str1 = "Strasse";
+            string str2 = "Straße";
+
+            bool areEqual = string.Equals(str1, str2, StringComparison.Ordinal);
+            Console.WriteLine(areEqual);
+            areEqual = string.Equals(str1, str2, StringComparison.InvariantCulture);
+            Console.WriteLine(areEqual);
+            areEqual = string.Equals(str1, str2, StringComparison.CurrentCulture);
+            Console.WriteLine(areEqual);
+        }
+        static void StringFormat()              //022 Форматирование строк.mp4              
+        {
+            string name = "John";
+            int age = 30;
+            string str1 = string.Format("My name is {0} and I'm {1}", name, age);
+            string str2 = $"My name is {name} and I'm {age}";
+
+            //string str2 = "My name is " + name + " and I'm " + age;
+
+            string str3 = "My name is \nJohn";
+            string str4 = "I'm \t30";
+
+            str3 = $"My name is {Environment.NewLine}John";
+
+            //string str5 = "I'm John and I'm a "good" programmer";
+            string str5 = "I'm John and I'm a \"good\" programmer";
+
+            //string str6 = "C:\\tmp\\test_file.txt";
+            string str6 = @"C:\tmp\test_file.txt";
+
+            double answer = 42.08;
+            //string result = string.Format("{0:d}", answer);
+            //string result2 = string.Format("{0:d4}", answer);
+
+            string result = string.Format("{0:f}", answer);
+            string result2 = string.Format("{0:f1}", answer);
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            double money = 12.8;
+            result = string.Format("{0:C}", money);
+            result2 = string.Format("{0:C2}", money);
+
+            result = $"{money:C2}";
+
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
+            Console.WriteLine(money.ToString("C2"));
+        }
+        static void StringBuilderDemo()         //021 StringBuilder.mp4                     
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My ");
+            sb.Append("name ");
+            sb.Append("is ");
+            sb.Append("John");
+            sb.AppendLine("!");
+            sb.AppendLine("Hello!");
+
+            string str = sb.ToString();
+            Console.WriteLine(str);
+        }
+        static void StringModification()        //020 Изменение строк.mp4                   
+        {
+            string nameConcat = string.Concat("My ", "name ", "is ", "John");
+            Console.WriteLine(nameConcat);
+
+            nameConcat = string.Join(" ", "My", "name", "is", "John");
+            Console.WriteLine(nameConcat);
+
+            nameConcat = "My " + "name " + "is " + "John";
+
+            //string newName = 
+            nameConcat = nameConcat.Insert(0, "By the way, ");
+            Console.WriteLine(nameConcat);
+
+            nameConcat = nameConcat.Remove(0, 1);
+            Console.WriteLine(nameConcat);
+
+            string replaced = nameConcat.Replace('n', 'z');
+            Console.WriteLine(replaced);
+
+            string data = "12;28;34;25;64";
+            string[] spliData = data.Split(';');
+            string first = spliData[0];
+            Console.WriteLine(first);
+
+            char[] chars = nameConcat.ToCharArray();
+            Console.WriteLine(chars[0]);
+            Console.WriteLine(nameConcat[0]);
+
+            string lower = nameConcat.ToLower();
+            Console.WriteLine(lower);
+
+            string upper = nameConcat.ToUpper();
+            Console.WriteLine(upper);
+
+            string john = " My name is John ";
+            Console.WriteLine(john.Trim());
+        }
+        static void StringEmptiness()           //019 Пустота строк.mp4                     
+        {
+            string str = string.Empty; //same as ""
+
+            string empty = "";
+            string whiteSpaced = " ";
+            string notEmpty = " b";
+            string nullString = null;
+
+            Console.WriteLine("IsNullOrEmpty");
+            bool isNullOrEmpty = string.IsNullOrEmpty(nullString);
+            Console.WriteLine(isNullOrEmpty);
+
+            isNullOrEmpty = string.IsNullOrEmpty(whiteSpaced);
+            Console.WriteLine(isNullOrEmpty);
+
+            isNullOrEmpty = string.IsNullOrEmpty(notEmpty);
+            Console.WriteLine(isNullOrEmpty);
+
+            isNullOrEmpty = string.IsNullOrEmpty(empty);
+            Console.WriteLine(isNullOrEmpty);
+
+            Console.WriteLine();
+            Console.WriteLine("IsNullOrWhiteSpace");
+
+            bool isNullOrWhiteSpace = string.IsNullOrWhiteSpace(nullString);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+            isNullOrWhiteSpace = string.IsNullOrWhiteSpace(whiteSpaced);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+            isNullOrWhiteSpace = string.IsNullOrWhiteSpace(notEmpty);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+            isNullOrWhiteSpace = string.IsNullOrWhiteSpace(empty);
+            Console.WriteLine(isNullOrWhiteSpace);
+        }
+        static void QueryingStrings()           //018 Базовый API для работы со строками.mp4
+        {
+            string name = "abracadabra";
+            bool containsA = name.Contains('a');
+            bool containsE = name.Contains('E');
+
+            Console.WriteLine(containsA);
+            Console.WriteLine(containsE);
+
+            bool endsWithAbra = name.EndsWith("abra");
+            Console.WriteLine(endsWithAbra);
+
+            bool startsWithAbra = name.StartsWith("abra");
+            Console.WriteLine(startsWithAbra);
+
+            int indexOfA = name.IndexOf('a', 1);
+            Console.WriteLine(indexOfA);
+
+            int lastIndexOfR = name.LastIndexOf('r');
+            Console.WriteLine(lastIndexOfR);
+
+            Console.WriteLine(name.Length);
+
+            string substrFrom5 = name.Substring(5);
+            string substrFromTo = name.Substring(0, 3);
+
+            Console.WriteLine(substrFrom5);
+            Console.WriteLine(substrFromTo);
+        }
+        static void StaticAndInstanceMembers()  //017 Экземплярные и статические методы.mp4 
+        {
+            string name = "abracadabra";
+            bool containsA = name.Contains('a');
+            bool containsE = name.Contains('E');
+
+            Console.WriteLine(containsA);
+            Console.WriteLine(containsE);
+
+            string abc = string.Concat("a", "b", "c");
+            Console.WriteLine(abc);
+
+            Console.WriteLine(int.MinValue);
+
+            int x = 1;
+            string xStr = x.ToString();
+            Console.WriteLine(xStr);
+            Console.WriteLine(x);
+        }
+        static void ComparisonOperators()       //016 Алгебраические операции.mp4 (3)       
+        {
+            int x = 1;
+            int y = 2;
+
+            bool areEqual = x == y;
+            Console.WriteLine(areEqual);
+
+            bool result = x > y;
+            Console.WriteLine(result);
+
+            result = x >= y;
+            Console.WriteLine(result);
+
+            result = x < y;
+            Console.WriteLine(result);
+
+            result = x <= y;
+            Console.WriteLine(result);
+
+            result = x != y;
+
+            Console.WriteLine(result);
+        }
+        static void MathOperations()            //016 Алгебраические операции.mp4 (2)       
+        {
+            int x = 1;
+            int y = 2;
+
+            int z = x + y;
+            int k = x - y;
+            int a = z + k - y;
+
+            Console.WriteLine(z);
+            Console.WriteLine(k);
+            Console.WriteLine(a);
+
+            int b = z * 2;
+            int c = k / 2;
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            a = 4 % 2;
+            b = 5 % 2;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            a = 3;
+            a = a * a;
+            //a = a * a * a;
+            Console.WriteLine(a);
+
+            a = (2 + 2) * 2;
+            Console.WriteLine(a);
+            a *= 2;
+            //a = a * 2
+            Console.WriteLine(a);
+            a /= 2;
+            //a = a / 2
+            Console.WriteLine(a);
+        }
+        static void IncrementDecrementDemo()    //016 Алгебраические операции.mp4 (1)       
         {
             int x = 1;
             x = x + 1;
@@ -37,14 +429,14 @@
             Console.WriteLine(x);
 
             x += 2;
-            // x = x + 2;
+            // x = x + 2
+            Console.WriteLine(x);
 
             j -= 2;
-            // j = j - 2;
-
+            //j = j - 2
             Console.WriteLine(j);
         }
-        static void Overflow()                  //015 Переполнение.mp4
+        static void Overflow()                  //015 Переполнение.mp4                      
         {
             checked
             {
@@ -59,7 +451,7 @@
                 Console.WriteLine(x);
             }
         }
-        static void VariablesScope()            //014 Область видимости переменных.mp4
+        static void VariablesScope()            //014 Область видимости переменных.mp4      
         {
             var a = 1;
             {
@@ -78,7 +470,7 @@
             //Console.WriteLine(b);
             //Console.WriteLine(c);
         }
-        static void Literals()                  //013 Литералы.mp4
+        static void Literals()                  //013 Литералы.mp4                          
         {
             int x = 0b11;
             int y = 0b1001;
@@ -115,7 +507,7 @@
             Console.WriteLine('\u0420');
             Console.WriteLine('\u0421');
         } 
-        static void Variables()                 //012 Создаём переменные.mp4
+        static void Variables()                 //012 Создаём переменные.mp4                
         {
             int x = -1;
 
