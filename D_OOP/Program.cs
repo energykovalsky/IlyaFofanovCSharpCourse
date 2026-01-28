7358f8a591750143ac51ef38b8576ee7529a3f9d
Andriy Kovalsky <energy.kovalsky@gmail.com>
2026-01-28 04:07:39 +0200

    Added ValRefTypes (video 075)

--- D_OOP/Program.cs
+++ D_OOP/Program.cs
@@ -1,50 +1,86 @@
-﻿namespace D_OOP;
+﻿using D_OOP;
+
+namespace D_OOP;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Character c = new Character();
-        c.Hit(120);
-        Console.WriteLine($"C.Health = {c.Health}");
-        Console.WriteLine();
+        PointVal a; //same as Point a = new PointVal();
+        a.X = 3;
+        a.Y = 5;
 
-        Calculator calc = new Calculator();
+        PointVal b = a;
+        b.X = 7;
+        b.Y = 10;
 
-        double Square1 = calc.CalcTriangleSquare(1, 1);
-        double Square2 = calc.CalcTriangleSquare(1, 1, 1);
-        
-        double Square3 = Calculator.CalcTriangleSquare(ab: 1, ac: 1, alpha: 1); //071 Именованные аргументы.mp4
-        double Square4 = Calculator.CalcTriangleSquare(ab: 1, ac: 1, alpha: 1, isInRadians: true); //Опциональные параметры
+        a.LogValues();
+        b.LogValues();
 
-        Console.WriteLine($"Square1 = {Square1}");
-        Console.WriteLine($"Square2 = {Square2}");
-        Console.WriteLine($"Square3 = {Square3}");
+        Console.WriteLine("After sturcts");
+        PointRef c = new PointRef();
+        c.X = 3;
+        c.Y = 5;
 
-        double avg = Calculator.Average2(1, 2, 3, 4);
-        Console.WriteLine(avg);
+        PointRef d = c;
+        d.X = 7;
+        d.Y = 10;
 
-        Console.WriteLine("Enter a number, please");
-        //int number = int.Parse(Console.ReadLine())
-        string line = Console.ReadLine();
-        bool wasParsed = int.TryParse(line, out int number);
-        if (wasParsed)
-        {
-            Console.WriteLine(number);
-        }
-        else
-        {
-            Console.WriteLine("Failed to parse");
-        }
-
-
-        if (Calculator.TryDivide(1, 0, out double result))
-        {
-            Console.WriteLine(result);
-        }
-        else
-        {
-            Console.WriteLine("Failed to parse");
-        }
+        c.LogValues();
+        d.LogValues();
     }
 }
+
+
+
+
+
+
+//        Character c = new Character();
+//        c.Hit(120);
+//        Console.WriteLine($"C.Health = {c.Health}");
+//        Console.WriteLine();
+
+
+
+
+//        Calculator calc = new Calculator();
+
+//double Square1 = calc.CalcTriangleSquare(1, 1);
+//double Square2 = calc.CalcTriangleSquare(1, 1, 1);
+
+//double Square3 = Calculator.CalcTriangleSquare(ab: 1, ac: 1, alpha: 1); //071 Именованные аргументы.mp4
+//double Square4 = Calculator.CalcTriangleSquare(ab: 1, ac: 1, alpha: 1, isInRadians: true); //Опциональные параметры
+
+//Console.WriteLine($"Square1 = {Square1}");
+//        Console.WriteLine($"Square2 = {Square2}");
+//        Console.WriteLine($"Square3 = {Square3}");
+
+//        double avg = Calculator.Average2(1, 2, 3, 4);
+//Console.WriteLine(avg);
+
+//        Console.WriteLine("Enter a number, please");
+//        //int number = int.Parse(Console.ReadLine())
+//        string line = Console.ReadLine();
+//bool wasParsed = int.TryParse(line, out int number);
+//        if (wasParsed)
+//        {
+//            Console.WriteLine(number);
+//        }
+//        else
+//{
+//    Console.WriteLine("Failed to parse");
+//}
+
+
+
+
+
+//if (Calculator.TryDivide(1, 0, out double result))
+//{
+//    Console.WriteLine(result);
+//}
+//else
+//{
+//    Console.WriteLine("Failed to parse");
+//}
---
+++ D_OOP/ValRefTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_OOP
+{
+    public struct PointVal
+    {
+        public int X;
+        public int Y;
+
+        public void LogValues()
+        {
+            Console.WriteLine($"X={X}; Y={Y}");
+        }
+    }
+
+    public class PointRef
+    {
+        public int X;
+        public int Y;
+
+        public void LogValues()
+        {
+            Console.WriteLine($"X={X}; Y={Y}");
+        }
+    }
+
+}
